c2188061c3968ac0e4758bc4125028f68e98a80b
Piatnytsia Oleksii <pyatnitsa90@gmail.com>
2023-04-19 12:39:50 +0300

    https://lms.ithillel.ua/groups/63c00b312482232c29370c19/homeworks/642310650246b334f6bbbef0

---
+++ src/main/java/com/hillel/piatnytsia/homeworks/homework7/coffee/order/CoffeeOrderBoard.java
@@ -0,0 +1,37 @@
+package com.hillel.piatnytsia.homeworks.homework7.coffee.order;
+
+import java.util.*;
+
+
+public class CoffeeOrderBoard {
+
+    Queue<Order> queue = new LinkedList<>() {
+    };
+
+
+    public void add(Order order) {
+        queue.add(order);
+    }
+
+    public void deliver() {
+        queue.poll();
+    }
+
+    public Order deliver(int numOrder) {
+        for (Order orderPickup : queue) {
+            if (orderPickup.getNumOrder() == numOrder) {
+                queue.remove(orderPickup);
+                return orderPickup;
+            }
+        }
+        return null;
+    }
+
+    public void draw() {
+        System.out.println("\nNum | Name: ");
+        for (Order order : queue) {
+            System.out.println(order.getNumOrder() + "   | " + order.getName());
+        }
+
+    }
+}
---
+++ src/main/java/com/hillel/piatnytsia/homeworks/homework7/coffee/order/Main.java
@@ -0,0 +1,31 @@
+package com.hillel.piatnytsia.homeworks.homework7.coffee.order;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Order orderCoffee = new Order("Alen");
+        Order orderCola = new Order("Roman");
+        Order orderBurger = new Order("Anton");
+        Order orderSalad = new Order("Dimon");
+        Order orderPonchik = new Order("Anna");
+
+        CoffeeOrderBoard coffeeOrderBoard = new CoffeeOrderBoard();
+        coffeeOrderBoard.add(orderCoffee);
+        coffeeOrderBoard.add(orderCola);
+        coffeeOrderBoard.add(orderBurger);
+        coffeeOrderBoard.add(orderSalad);
+        coffeeOrderBoard.add(orderPonchik);
+
+        System.out.printf("\n" + coffeeOrderBoard.queue + "\n");
+
+        coffeeOrderBoard.deliver();
+        System.out.printf("\n" + coffeeOrderBoard.queue + "\n");
+
+        coffeeOrderBoard.deliver(4);
+        System.out.printf("\n" + coffeeOrderBoard.queue + "\n");
+
+        coffeeOrderBoard.draw();
+    }
+}
