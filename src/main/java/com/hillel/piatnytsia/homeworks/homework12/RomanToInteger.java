c079c1fe8d79bb634b5520e5e21e800af7625c4d
Piatnytsia Oleksii <pyatnitsa90@gmail.com>
2023-05-30 22:23:58 +0300

    https://lms.ithillel.ua/groups/63c00b312482232c29370c19/homeworks/643db54e49d3c13a47c7b2b6

--- src/main/java/com/hillel/piatnytsia/homeworks/homework12/RomanToInteger.java
+++ src/main/java/com/hillel/piatnytsia/homeworks/homework12/RomanToInteger.java
@@ -8,20 +8,17 @@ public class RomanToInteger {
         Map<Character, Integer> romanNumbers = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
         int previousValue = 0;
         int result = 0;
-        int count = 0;
         char[] inputValue = s.toCharArray();
         for (char c : inputValue) {
             if (romanNumbers.containsKey(c)) {
                 Integer value = romanNumbers.get(c);
-                if (count == 0 || value <= previousValue) {
+                if (result == 0 || value <= previousValue) {
                     result += value;
                     previousValue = value;
-                    count++;
                 } else {
-
                     result += value - 2 * previousValue;
                     previousValue = value;
-                    count++;
+
                 }
             }
         }
