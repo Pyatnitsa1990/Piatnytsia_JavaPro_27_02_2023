19b1fb589987e5bee698f0b9396f52ead9504e9b
Piatnytsia Oleksii <pyatnitsa90@gmail.com>
2023-05-30 22:22:59 +0300

    https://lms.ithillel.ua/groups/63c00b312482232c29370c19/homeworks/643db54e49d3c13a47c7b2b6

--- src/main/java/com/hillel/piatnytsia/homeworks/homework12/RomanToInteger.java
+++ src/main/java/com/hillel/piatnytsia/homeworks/homework12/RomanToInteger.java
@@ -5,27 +5,27 @@ import java.util.Map;
 
 public class RomanToInteger {
     public static int romanToInt(String s) {
-        Map<Character, Integer> romanNumbers = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100,'D', 500, 'M', 1000);
+        Map<Character, Integer> romanNumbers = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
         int previousValue = 0;
         int result = 0;
+        int count = 0;
         char[] inputValue = s.toCharArray();
-        for (int i = 0; i < inputValue.length; i++) {
-            for (Map.Entry<Character, Integer> romanNumberEntry : romanNumbers.entrySet()) {
+        for (char c : inputValue) {
+            if (romanNumbers.containsKey(c)) {
+                Integer value = romanNumbers.get(c);
+                if (count == 0 || value <= previousValue) {
+                    result += value;
+                    previousValue = value;
+                    count++;
+                } else {
 
-                Character key = romanNumberEntry.getKey();
-                if (key.equals(inputValue[i])) {
-                    Integer value = romanNumberEntry.getValue();
-                    if (i==0 || value <=previousValue){
-                        result+=value;
-                        previousValue = value;
-                    } else{
-
-                        result+=value-2*previousValue;
-                        previousValue = value;
-                    }
+                    result += value - 2 * previousValue;
+                    previousValue = value;
+                    count++;
                 }
             }
         }
+
         System.out.println(result);
         return result;
     }
