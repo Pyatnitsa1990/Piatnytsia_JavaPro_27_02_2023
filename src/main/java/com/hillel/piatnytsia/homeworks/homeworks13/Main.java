c850eac51095fa8232b901f6fc66dc42559dfd39
Piatnytsia Oleksii <pyatnitsa90@gmail.com>
2023-06-03 12:18:39 +0300

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/

---
+++ src/main/java/com/hillel/piatnytsia/homeworks/homeworks13/Main.java
@@ -0,0 +1,8 @@
+package com.hillel.piatnytsia.homeworks.homeworks13;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] prices = {3,3,5,0,0,3,1,4};
+        System.out.println(BuyTime.maxProfit(prices));
+    }
+}
